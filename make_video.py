16c863beaa648b48fd55092968334766df4f1073
FredThx <fredthx@gmail.com>
2019-02-10 18:13:36 +0100

    make_video

--- make_video.py
+++ make_video.py
@@ -2,62 +2,29 @@
 # -*- coding: utf-8 -*
 
 import sys, os, time
-import pymedia.video.vcodec as vcodec
-import pygame
+import pathlib
+import cv2
 
 def makeVideo( inPattern, outFile, outCodec ):
-  # Get video stream dimensions from the image size
-
-  pygame.init()
-  i= 1
-  # Opening mpeg file for output
-
-  e= None
-  i= 1
-  fw= open( outFile, 'wb' )
-  while i:
-    if os.path.isfile( inPattern % i ):
-      s= pygame.image.load( inPattern % i )
-      if not e:
-        if outCodec== 'mpeg1video':
-          bitrate= 2700000
-        else:
-          bitrate= 9800000
-        
-        params= { \
-          'type': 0,
-          'gop_size': 12,
-          'frame_rate_base': 125,
-          'max_b_frames': 0,
-          'height': s.get_height(),
-          'width': s.get_width(),
-          'frame_rate': 2997,
-          'deinterlace': 0,
-          'bitrate': bitrate,
-          'id': vcodec.getCodecID( outCodec )
-        }
-        print 'Setting codec to ', params
-        e= vcodec.Encoder( params )
-        t= time.time()
-      
-      # Create VFrame
-
-      ss= pygame.image.tostring(s, "RGB")
-      bmpFrame= vcodec.VFrame( vcodec.formats.PIX_FMT_RGB24, s.get_size(), (ss,None,None))
-      yuvFrame= bmpFrame.convert( vcodec.formats.PIX_FMT_YUV420P )
-      d= e.encode( yuvFrame )
-      fw.write( d )
-      i+= 1
-    else:
-      print '%d frames written in %.2f secs( %.2f fps )' % ( i, time.time()- t, float( i )/ ( time.time()- t ) )
-      i= 0
-  
-  fw.close()
-  pygame.quit()
-
+    # Get video stream dimensions from the image size
+    print("MakeVideo : %s => %s (%s)"%(inPattern, outFile, outCodec))
+    inPattern = pathlib.Path(inPattern)
+    image = cv2.imread(str(inPattern / os.listdir(str(inPattern))[0]))
+    height, width, channel = image.shape
+    print("Dimension detected : %s-%s"%(width,height))
+    video = cv2.VideoWriter(outFile,-1,30.0,(width,height), True)
+    for file in os.listdir(str(inPattern)):
+        print(str(inPattern / file))
+        try:
+            image = cv2.imread(str(inPattern / file))
+            image = cv2.resize(image, (width,height))
+            video.write(image)
+        except Exception  as e:
+            print(str(e))
+    video.release()
 
 if __name__== '__main__':
-  if len( sys.argv )!= 4:
-    print "Usage: make_video <in_file_pattern> <out_file> <format>\n\tformat= { mpeg1video | mpeg2video }"
-  else:
-    makeVideo( sys.argv[ 1 ], sys.argv[ 2 ], sys.argv[ 3 ] )
+    if len( sys.argv )!= 4:
+        print "Usage: make_video <in_file_pattern> <out_file> <format>\n\tformat= { mpeg1video | mpeg2video }"
+    else:
+        makeVideo( sys.argv[ 1 ], sys.argv[ 2 ], sys.argv[ 3 ] )
